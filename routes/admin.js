9f09f207f4fb42a08d7ad1af2d3b307f964ec5ea
ishrat2004 <ishratalikhan004@gmail.com>
2025-05-21 19:01:43 +0530

    Added the Signin and Signup

--- routes/admin.js
+++ routes/admin.js
@@ -1,16 +1,49 @@
 const Router=require("express"); 
 const adminRouter=Router(); 
-const {adminModel}=require("../db")
-    adminRouter.post("/signup",(req,res)=>{ 
+const {adminModel,userModel}=require("../db") 
+const jwt=require("jsonwebtoken"); 
+const JWT_SECRET="admincsa";  
+    adminRouter.post("/signup",async(req,res)=>{  
+        const email=req.body.email; 
+        const password=req.body.password; 
+        const firstName=req.body.firstname; 
+        const lastName=req.body.secondname;   
+        // add try catch and add hashed pwd and add zod 
+        const p=await adminModel.create({ 
+            email:email,
+            password:password,
+            firstName:firstName,
+            lastName:lastName
+        })
         res.json({ 
-            msg:"user signup endpoints"
+            msg:" signup done "
         })
     }); 
 
-    adminRouter.post("/signin",(req,res)=>{ 
-        res.json({ 
-            msg:"user signin endpoint"
-        })
+    adminRouter.post("/signin",async(req,res)=>{ 
+        const {email,password}=req.body;
+               //if u have stored hashed pwd then u cannot compare password  
+               let admin=await adminModel.findOne({ 
+                   email:email,
+                   password:password
+               })
+              ////.findOne returns either that entry or undefined
+              //// .find returns empty array it will be considered as true,u can check by admin.length==1 
+               if(admin){ 
+                     const token=jwt.sign({ 
+                       id:admin._id
+                     },JWT_SECRET); 
+                     /// cookie based auth can be added here 
+                     res.json({ 
+                       token:token,
+                       msg:"sign up successfully "
+                     })
+               }else{ 
+               res.status(403).json({ 
+                   msg:"Invalid cred"
+               }) 
+              }
+       
     }); 
     
     adminRouter.post("/course",function(req,res){ 
--- routes/user.js
+++ routes/user.js
@@ -1,16 +1,51 @@
 const express=require('express'); 
 const Router=express.Router;
 const userRouter = Router(); 
-    userRouter.post("/signup",(req,res)=>{ 
+const {adminModel,userModel}=require("../db") 
+const jwt=require("jsonwebtoken"); 
+const JWT_SECRET="usercsa"; 
+
+
+    userRouter.post("/signup",async (req,res)=>{ 
+        const email=req.body.email; 
+        const password=req.body.password; 
+        const firstName=req.body.firstname; 
+        const lastName=req.body.secondname;   
+        // add try catch and add hashed pwd and add zod 
+        const p=await userModel.create({ 
+            email:email,
+            password:password,
+            firstName:firstName,
+            lastName:lastName
+        })
         res.json({ 
-            msg:"user signup endpoints"
+            msg:" signup done "
         })
     }); 
 
-    userRouter.post("/signin",(req,res)=>{ 
-        res.json({ 
-            msg:"user signin endpoint"
+    userRouter.post("/signin",async (req,res)=>{  
+        const {email,password}=req.body;
+        //if u have stored hashed pwd then u cannot compare password  
+        let user=await userModel.findOne({ 
+            email:email,
+            password:password
         })
+       ////.findOne returns either that entry or undefined
+       //// .find returns empty array it will be considered as true,u can check by user.length==1 
+        if(user){ 
+              const token=jwt.sign({ 
+                id:user._id
+              },JWT_SECRET); 
+              /// cookie based auth can be added here 
+              res.json({ 
+                token:token,
+                msg:"sign up successfully "
+              })
+        }else{ 
+        res.status(403).json({ 
+            msg:"Invalid cred"
+        }) 
+       }
     });  
 
     userRouter.get("/purchases",(req,res)=>{ 
